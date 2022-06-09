5b5d0cbb45ea1e8f2510d4f751480b02fe43f8bf
Rishikesh-278 <rishikeshbidkar04@gmail.com>
2022-06-09 13:19:01 +0530

    no message

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -43,11 +45,7 @@ class _QuizPageState extends State<QuizPage> {
 
   // Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
 
-  List <Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-    Question(q: 'A slug\'s blood is green.', a: true)
-  ];
+
 
   int QuestionNumber = 0;
 
@@ -63,7 +61,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-              questionBank[QuestionNumber].questionText,
+                quizBrain.questionBank[QuestionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -88,7 +86,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
 
-                bool correctAnswer = questionBank[QuestionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.questionBank[QuestionNumber].questionAnswer;
 
                 if(correctAnswer == true)
                   {
@@ -120,7 +118,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
 
-                bool correctAnswer = questionBank[QuestionNumber].questionAnswer ;
+                bool correctAnswer = quizBrain.questionBank[QuestionNumber].questionAnswer ;
 
                 if(correctAnswer == false)
                 {
